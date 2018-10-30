26bf62ef5d92a7e027c556b819f83395985e2cb5
Trace Rainbolt <tracerainbolt@gmail.com>
2018-10-29 18:35:41 -0700

    Object detection.

---
+++ object_detection/detect_objects.py
@@ -0,0 +1,237 @@
+#!/usr/bin/env python
+import numpy as np
+import os
+import six.moves.urllib as urllib
+import sys
+import tarfile
+import tensorflow as tf
+import zipfile
+
+from distutils.version import StrictVersion
+from collections import defaultdict
+from io import StringIO
+from matplotlib import pyplot as plt
+from PIL import Image
+
+if StrictVersion(tf.__version__) < StrictVersion('1.9.0'):
+  raise ImportError('Please upgrade your TensorFlow installation to v1.9.* or later!')
+
+from utils import label_map_util
+from utils import visualization_utils as vis_util
+
+
+# # Model preparation 
+
+# ## Variables
+
+# What model to download.
+MODEL_NAME = 'sea_inference_graph'
+
+# Path to frozen detection graph. This is the actual model that is used for the object detection.
+PATH_TO_FROZEN_GRAPH = MODEL_NAME + '/frozen_inference_graph.pb'
+
+# List of the strings that is used to add correct label for each box.
+PATH_TO_LABELS = os.path.join('data', 'object_detection.pbtxt')
+
+# ## Load a (frozen) Tensorflow model into memory.
+
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+  od_graph_def = tf.GraphDef()
+  with tf.gfile.GFile(PATH_TO_FROZEN_GRAPH, 'rb') as fid:
+    serialized_graph = fid.read()
+    od_graph_def.ParseFromString(serialized_graph)
+    tf.import_graph_def(od_graph_def, name='')
+
+# ## Loading label map
+
+category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS, use_display_name=True)
+
+
+# ## Helper code
+
+def load_image_into_numpy_array(image):
+  (im_width, im_height) = image.size
+  return np.array(image.getdata()).reshape(
+      (im_height, im_width, 3)).astype(np.uint8)
+
+
+# # Detection
+PRESENT_MODE = '-p' in sys.argv
+PATH_TO_TEST_IMAGES_DIR = 'test_images'
+NUM_TEST_IMAGES = 30
+
+if PRESENT_MODE:
+    GOOD_EXAMPLES = [3, 13, 17, 19, 35, 53, 67, 68, 72, 75]
+    NUM_TEST_IMAGES = len(GOOD_EXAMPLES)
+    TEST_IMAGE_PATHS = [ os.path.join(PATH_TO_TEST_IMAGES_DIR, 'image{}.jpg'.format(i)) for i in GOOD_EXAMPLES]
+else:
+    TEST_IMAGE_PATHS = [ os.path.join(PATH_TO_TEST_IMAGES_DIR, 'image{}.jpg'.format(i)) for i in range(1, NUM_TEST_IMAGES + 1)]
+
+# Size, in inches, of the output images.
+IMAGE_SIZE = (12, 8)
+
+def run_inference_for_single_image(image, graph):
+  with graph.as_default():
+    with tf.Session() as sess:
+      # Get handles to input and output tensors
+      ops = tf.get_default_graph().get_operations()
+      all_tensor_names = {output.name for op in ops for output in op.outputs}
+      tensor_dict = {}
+      for key in [
+          'num_detections', 'detection_boxes', 'detection_scores',
+          'detection_classes', 'detection_masks'
+      ]:
+        tensor_name = key + ':0'
+        if tensor_name in all_tensor_names:
+          tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(
+              tensor_name)
+      if 'detection_masks' in tensor_dict:
+        # The following processing is only for single image
+        detection_boxes = tf.squeeze(tensor_dict['detection_boxes'], [0])
+        detection_masks = tf.squeeze(tensor_dict['detection_masks'], [0])
+        # Reframe is required to translate mask from box coordinates to image coordinates and fit the image size.
+        real_num_detection = tf.cast(tensor_dict['num_detections'][0], tf.int32)
+        detection_boxes = tf.slice(detection_boxes, [0, 0], [real_num_detection, -1])
+        detection_masks = tf.slice(detection_masks, [0, 0, 0], [real_num_detection, -1, -1])
+        detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
+            detection_masks, detection_boxes, image.shape[0], image.shape[1])
+        detection_masks_reframed = tf.cast(
+            tf.greater(detection_masks_reframed, 0.5), tf.uint8)
+        # Follow the convention by adding back the batch dimension
+        tensor_dict['detection_masks'] = tf.expand_dims(
+            detection_masks_reframed, 0)
+      image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
+
+      # Run inference
+      output_dict = sess.run(tensor_dict,
+                             feed_dict={image_tensor: np.expand_dims(image, 0)})
+
+      # all outputs are float32 numpy arrays, so convert types as appropriate
+      output_dict['num_detections'] = int(output_dict['num_detections'][0])
+      output_dict['detection_classes'] = output_dict[
+          'detection_classes'][0].astype(np.uint8)
+      output_dict['detection_boxes'] = output_dict['detection_boxes'][0]
+      output_dict['detection_scores'] = output_dict['detection_scores'][0]
+      if 'detection_masks' in output_dict:
+        output_dict['detection_masks'] = output_dict['detection_masks'][0]
+  return output_dict
+
+import matplotlib.widgets
+import matplotlib.patches
+import mpl_toolkits.axes_grid1
+
+class PageSlider(matplotlib.widgets.Slider):
+
+    def __init__(self, ax, label, numpages = 10, valinit=0, valfmt='%1d', 
+                 closedmin=True, closedmax=True,  
+                 dragging=True, **kwargs):
+
+        self.facecolor=kwargs.get('facecolor',"w")
+        self.activecolor = kwargs.pop('activecolor',"b")
+        self.fontsize = kwargs.pop('fontsize', 10)
+        self.numpages = numpages
+
+        super(PageSlider, self).__init__(ax, label, 0, numpages, 
+                            valinit=valinit, valfmt=valfmt, **kwargs)
+
+        self.poly.set_visible(False)
+        self.vline.set_visible(False)
+        self.pageRects = []
+        for i in range(numpages):
+            facecolor = self.activecolor if i==valinit else self.facecolor
+            r  = matplotlib.patches.Rectangle((float(i)/numpages, 0), 1./numpages, 1, 
+                                transform=ax.transAxes, facecolor=facecolor)
+            ax.add_artist(r)
+            self.pageRects.append(r)
+            ax.text(float(i)/numpages+0.5/numpages, 0.5, str(i+1),  
+                    ha="center", va="center", transform=ax.transAxes,
+                    fontsize=self.fontsize)
+        self.valtext.set_visible(False)
+
+        divider = mpl_toolkits.axes_grid1.make_axes_locatable(ax)
+        bax = divider.append_axes("right", size="5%", pad=0.05)
+        fax = divider.append_axes("right", size="5%", pad=0.05)
+        self.button_back = matplotlib.widgets.Button(bax, label='<', 
+                        color=self.facecolor, hovercolor=self.activecolor)
+        self.button_forward = matplotlib.widgets.Button(fax, label='>', 
+                        color=self.facecolor, hovercolor=self.activecolor)
+        self.button_back.label.set_fontsize(self.fontsize)
+        self.button_forward.label.set_fontsize(self.fontsize)
+        self.button_back.on_clicked(self.backward)
+        self.button_forward.on_clicked(self.forward)
+
+    def _update(self, event):
+        super(PageSlider, self)._update(event)
+        i = int(self.val)
+        if i >=self.valmax:
+            return
+        self._colorize(i)
+
+    def _colorize(self, i):
+        for j in range(self.numpages):
+            self.pageRects[j].set_facecolor(self.facecolor)
+        self.pageRects[i].set_facecolor(self.activecolor)
+
+    def forward(self, event):
+        current_i = int(self.val)
+        i = current_i+1
+        if (i < self.valmin) or (i >= self.valmax):
+            return
+        self.set_val(i)
+        self._colorize(i)
+
+    def backward(self, event):
+        current_i = int(self.val)
+        i = current_i-1
+        if (i < self.valmin) or (i >= self.valmax):
+            return
+        self.set_val(i)
+        self._colorize(i)
+
+def show_image(image_path):
+    image = Image.open(image_path)
+    # the array based representation of the image will be used later in order to prepare the
+    # result image with boxes and labels on it.
+    image_np = load_image_into_numpy_array(image)
+    # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+    image_np_expanded = np.expand_dims(image_np, axis=0)
+    # Actual detection.
+    output_dict = run_inference_for_single_image(image_np, detection_graph)
+    # Visualization of the results of a detection.
+    vis_util.visualize_boxes_and_labels_on_image_array(
+      image_np,
+      output_dict['detection_boxes'],
+      output_dict['detection_classes'],
+      output_dict['detection_scores'],
+      category_index,
+      instance_masks=output_dict.get('detection_masks'),
+      use_normalized_coordinates=True,
+      line_thickness=8)
+    return image_np
+
+
+fig, ax = plt.subplots()
+# plt.figure(figsize=IMAGE_SIZE)
+fig.subplots_adjust(bottom=0.18)
+ax_slider = fig.add_axes([0.1, 0.05, 0.8, 0.04])
+
+images = []
+if PRESENT_MODE:
+    for img in TEST_IMAGE_PATHS:
+        images.append(show_image(img))
+    im = ax.imshow(images[0])
+else:
+    im = ax.imshow(show_image(TEST_IMAGE_PATHS[0]))
+
+slider = PageSlider(ax_slider, 'Page', NUM_TEST_IMAGES, activecolor="orange")
+def update(val):
+    i = int(slider.val)
+    if PRESENT_MODE:
+        image = images[i]
+    else:
+        image = show_image(TEST_IMAGE_PATHS[i])
+    im.set_data(image)
+
+slider.on_changed(update)
+plt.show()
